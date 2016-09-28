a2cdfac1b99aeb758136215735d0ba85aface3e2
caleb from home desk <caleb@caleb.com>
2016-09-27 22:56:31 -0700

    Started GruntCheck npm test not working. Also finished JSObjs

--- WeekO2-JavaScriptObjects/work.js
+++ WeekO2-JavaScriptObjects/work.js
@@ -6,9 +6,40 @@ var person = {
 	}
 }
 
+var calculator = {
+	operator01: -1,
+	operator02: -1,
+	add: function() {
+		return this.operator01 + this.operator02;
+	},
+	subtract: function() {
+		return this.operator01 - this.operator02;
+	},
+	multiply: function() {
+			return this.operator01 * this.operator02;
+	}
+}
+
+function divider(title) {
+    console.log("====================================");
+    console.log(title);
+    console.log("====================================");
+}
 
 
+divider('Person');
 console.log(person.firstName);
 console.log(person.lastName);
 console.log(person.fullName());
 	
+
+calculator.operator01 = person.firstName.length;
+calculator.operator02 = person.lastName.length;
+
+divider('Calculator');
+console.log('operator01 =', calculator.operator01);
+console.log('operator01 =', calculator.operator02);
+
+console.log('Add: ' + calculator.add());
+console.log('Subtract: ' + calculator.subtract());
+console.log('Multiply: ' + calculator.multiply());
