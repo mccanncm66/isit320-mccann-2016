e2d5519ae4493a51ba84c245ac90f90f84f9b4de
caleb from home desk <caleb@caleb.com>
2016-10-07 00:12:22 -0700

    added Week03-ExpressJQuery finished

---
+++ Week03-ExpressJQuery/public/javascripts/Control.js
@@ -0,0 +1,50 @@
+/**
+ * Created by bcuser on 10/6/16.
+ */
+var MyObject = (function() {
+
+    // constructor
+    function MyObject() {
+        $('#sendString').click(showString);
+        $('#getItems').click(showItems);
+        $('#getMarie').click(showMarie);
+    }
+
+    function showString() {
+        $('#stringHolder').html('Send string was clicked');
+    }
+
+    function showItems() {
+        $("#myList").append('<li>' + 'item01' + '</li>');
+        $("#myList").append('<li>' + 'item02' + '</li>');
+        $("#myList").append('<li>' + 'item03' + '</li>');
+    }
+
+    function showMarie() {
+        var marie = {
+            "firstName": "Marie",
+            "lastName": "Curie",
+            "city": "Paris",
+            "country": "France"
+        };
+
+        for (var property in marie) {
+            if (marie.hasOwnProperty(property)) {
+                $("#myList").append('<li>' + marie[property] + '</li>');
+            }
+        }
+
+    }
+
+    MyObject.prototype.readyCalled = function() {
+        $("#readyCalled").html("Ready was called and myObjected created");
+    };
+
+    return MyObject;
+}());
+
+
+$(document).ready(function() {
+    var myObject = new MyObject();
+    myObject.readyCalled();
+});
