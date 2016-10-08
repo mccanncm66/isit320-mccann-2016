1ea04bd9a1f5ee0293bd45e53df078fdb20b5c6d
caleb from home desk <caleb@caleb.com>
2016-10-08 00:45:07 -0700

    fixed title wk03 expressRoutes

--- Week03-ExpressRoutes/routes/index.js
+++ Week03-ExpressRoutes/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     'use strict';
     res.render('index', {
-        title: 'Express'
+        title: 'McCann-ExpressRoutes-2016'
     });
 });
 
