2f10ba44ecebbb1825ae630e31439e76ce3983ee
caleb from home desk <caleb@caleb.com>
2016-10-21 22:17:45 -0700

    added favicon.png

--- Week05-DataMaster/routes/Couch.js
+++ Week05-DataMaster/routes/Couch.js
@@ -7,7 +7,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-var servers = [ 'http://168.156.46.218:5984',
+var servers = [ 'http://192.168.0.6:5984',
                 'http://127.0.0.1:5984',
                 'http://192.168.2.19:5984'];
 var serverIndex = 0;
