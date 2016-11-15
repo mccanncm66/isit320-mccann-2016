6e1f09631c963ca368be4a8403477ab11cfe8d9b
Charlie Calvert at school <foo@foobar.com>
2016-11-14 17:27:11 -0800

    started session basics

---
+++ Week09-SessionBasics/public/javascripts/control.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    'use strict';
+
+    function showPage(pageRoute) {
+
+        $.getJSON(pageRoute, function(data) {
+            console.log(data);
+            $('#display').html(JSON.stringify(data, null, 4));
+        }).fail(function(jq, textStatus, error) {
+            $('#displayArea').html('error: ' + jq.responseText);
+            console.log('error: ', status);
+            console.log('error: ', error);
+        });
+
+    }
+
+    $('#page01').click(function() {
+        showPage('/page01');
+    });
+    $('#page02').click(function(){
+        showPage('/page02');
+    });
+    $('#page03').click(function() {
+        showPage('/page03')
+    });
+    $('#viewPage01').click(function() {
+        showPage('/views/page01');
+    });
+    $('#viewPage02').click(function(){
+        showPage('/views/page02');
+    });
+
+});
+
+
+
+
+
+
