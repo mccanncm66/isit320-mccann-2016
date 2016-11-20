0f892bf67ba8caa41de3155099e39ef2742bbbb4
caleb from home desk <caleb@caleb.com>
2016-11-20 11:03:46 -0800

    Finished noderoutebasics NEEDS GC

--- Week09-NodeRouteBasics/public/javascripts/control.js
+++ Week09-NodeRouteBasics/public/javascripts/control.js
@@ -1,19 +1,37 @@
+
 $(document).ready(function() {
     'use strict';
+    var isValid;
+    var validateInput = function(userInput) {
+        if (parseInt(userInput)) {
+            return true;
+        } else {
+            $('#debug').html('Error: Please enter a number!');
+            return false;
+        }
+    };
+
+    $('button').click(function() {
+        isValid = validateInput($('#milesInput').val());
+    });
+
     $.postJSON = function(url, data, func) {
         $.post(url, data, func, 'json');
     };
+
     $('#feetToMiles').click(function() {
         console.log('Clicked on Feet To Miles');
         var miles = $('#milesInput').val();
         var input = {
             miles: miles
         };
-        $.getJSON('/feetToMiles', input, function(data) {
-            var output = JSON.stringify(data.result, null, 4);
-            console.log(output);
-            $('#debug').html(output);
-        });
+        if (isValid) {
+            $.getJSON('/feetToMiles', input, function (data) {
+                var output = JSON.stringify(data.result, null, 4);
+                console.log(output);
+                $('#debug').html(output);
+            });
+        }
     });
 
     $('#getFeet').click(function() {
@@ -27,11 +45,13 @@ $(document).ready(function() {
         var input = {
             radius: radius
         };
-        $.postJSON('/getCircumference', input, function(data) {
-            var output = JSON.stringify(data.result, null, 4);
-            console.log(output);
-            $('#debug').html(output);
-        });
+        if (isValid) {
+            $.postJSON('/getCircumference', input, function (data) {
+                var output = JSON.stringify(data.result, null, 4);
+                console.log(output);
+                $('#debug').html(output);
+            });
+        }
     });
 
     $('#postMilesToFeet').click(function() {
@@ -40,9 +60,12 @@ $(document).ready(function() {
             result: 'success',
             miles: miles
         };
-        $.getJSON('/miles-to-feet', data, function(data) {
-            var output = JSON.stringify(data.result, null, 4);
-            $('#debug').html(output);
-        });
+        if (isValid) {
+            $.getJSON('/miles-to-feet', data, function (data) {
+                var output = JSON.stringify(data.result, null, 4);
+                $('#debug').html(output);
+            });
+        }
     });
+
 });
