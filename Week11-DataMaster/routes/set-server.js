7c9ef5f9f7f13c33b1a04cbd12dc1158aa8c9a79
caleb from home desk <caleb@caleb.com>
2016-12-06 23:04:44 -0800

    changed ip for master

--- Week11-DataMaster/routes/set-server.js
+++ Week11-DataMaster/routes/set-server.js
@@ -3,13 +3,14 @@ var servers = ['http://127.0.0.1:5984',
     'http://168.156.47.121:5984',
     'http://192.168.0.6:5984',
     'http://168.156.47.122:5984/',
-    '168.156.47.122'
+    '168.156.47.122',
+    '127.0.0.1'
 
 ];
-var serverIndex = 4;
+var serverIndex = 0;
 var serverUrls = {
     dataServer: servers[serverIndex],
-    sessionServer: servers[5]
+    sessionServer: servers[6]
 };
 console.log('Middleware attaching to database on: ', serverUrls.dataServer);
 
