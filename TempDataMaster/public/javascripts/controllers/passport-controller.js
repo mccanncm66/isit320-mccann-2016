2a3022b8747cfe02167e102b40f259a635c710b4
caleb <caleb.mccann@bellevuecollege.edu>
2016-12-08 14:14:13 -0800

    melded changes

---
+++ TempDataMaster/public/javascripts/controllers/passport-controller.js
@@ -0,0 +1,34 @@
+/**
+ * Created by charlie on 11/26/16.
+ */
+
+define(['runQuery'], function(runQuery) {
+    'use strict';
+
+    function init() {
+        $('#aboutCharlie').click(function() {
+            $('#charlie').html('This is a note about Charlie made visible when the user clicked the button.');
+        });
+    }
+
+    var passportController = function(query, result) {
+        init();
+        var debug = $('#debug');
+        if (result.ok) {
+            var text = 'It worked';
+            if (result.data) {
+                text += '\n' + JSON.stringify(result.data, null, 4);
+            }
+            debug.html(text);
+        } else if (result.requestFailed) {
+            debug.html(JSON.stringify(result.requestFailed, null, 4));
+        }
+    };
+
+    passportController.about = function($q) {
+        return runQuery('/aboutText', $q);
+    };
+
+    return passportController;
+
+});
---
+++ TempDataMaster/routes/set-db.js
@@ -0,0 +1,5 @@
+var databaseName = 'couch-final-mccann';
+
+console.log('Middleware attaching to database on: ', databaseName);
+
+module.exports.databaseName = databaseName;
