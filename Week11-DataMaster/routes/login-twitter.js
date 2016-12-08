bcc455c9df9b482d5233c36176664636c6d4a401
Charlie Calvert at school <foo@foobar.com>
2016-12-07 16:15:01 -0800

    fixed typo

--- Week11-DataMaster/routes/login-twitter.js
+++ Week11-DataMaster/routes/login-twitter.js
@@ -39,7 +39,7 @@ router.get('/callback',
     });
 
 router.get('/profile', function(req, res) {
-    'use strict';z
+    'use strict';
     console.log(twitterProfile);
     res.render('profile-twitter', {
         title: 'Twitter Account',
